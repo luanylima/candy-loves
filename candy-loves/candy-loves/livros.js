f4a785d3acb7c2d572e0c846511a78e1ce36fbd5
luanyy <luany.lima@escola.pr.gov.br>
2026-06-02 08:34:25 -0300

    Delete candy-loves/candy-loves directory

--- candy-loves/candy-loves/livros.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.comprar').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Este é um projeto fictício! O livro não pode ser comprado.');
-    });
-});
